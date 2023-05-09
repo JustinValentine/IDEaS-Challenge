c7d8112b8f0f9b923f2f3eab45be501344d1d6c5
Justin Valentine <JustinValentineCS@gmail.com>
2023-05-09 14:06:30 -0600

    test

--- control/src/control.py
+++
@@ -1,6 +0,0 @@
-class ControlMethod:
-    def __init__(self, args):
-        pass
-    
-    def get_action(self, state):
-      raise NotImplementedError() 
--- perception/src/perception.py
+++
@@ -1,6 +0,0 @@
-class Perception:
-    def __init__(self, args):
-        pass
-    
-    def get_state(self, raw_images):
-      raise NotImplementedError() 
